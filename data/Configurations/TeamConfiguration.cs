00b7d0775d6f5d5a8c48d3db5519d5672671da97
med mokhtar ammar <35976850+MedMokhtarAmmar@users.noreply.github.com>
2019-02-27 19:53:35 +0100

    f

---
+++ data/Configurations/TeamConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data.Configurations
+{
+  public  class TeamConfiguration: EntityTypeConfiguration<Team>
+    {
+        public TeamConfiguration()
+        {
+            HasRequired<Bibliotheque>(t => t.Bibliotheque).WithMany(t => t.Documents).HasForeignKey(t => t.BibliothequeFK);
+        }
+    }
+}
